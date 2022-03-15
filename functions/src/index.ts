75549e600f93d52eb8af74bc10d793496502afa7
komekami <komekami0131@gmail.com>
2022-03-15 09:21:34 +0900

    feat:テストツイート

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
 import * as functions from "firebase-functions";
+import TwitterApi from "twitter-api-v2";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const twitterClient = new TwitterApi({
+  // /functions/.env
+  // HACK:undefined型を回避するため変数展開
+  appKey: `${process.env.APP_KEY}`,
+  appSecret: `${process.env.APP_SECRET}`,
+  accessToken: `${process.env.ACCESS_TOKEN}`,
+  accessSecret: `${process.env.ACCESS_SECRET}`,
+});
+
+export const tweet = functions.https.onRequest(async(request, response) => {
+  // WARN:同じ内容をツイートするとAPIの仕様によりエラー
+  await twitterClient.v2.tweet('This is test - v2');
+});
