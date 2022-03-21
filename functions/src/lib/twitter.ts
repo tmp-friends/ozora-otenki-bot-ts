53033fd2eb174fdc606f4bbba914104f9bd04f6d
komekami <komekami0131@gmail.com>
2022-03-21 14:56:22 +0900

    feat:画像付きでツイートする処理を実装

--- functions/src/lib/twitter.ts
+++ functions/src/lib/twitter.ts
@@ -1,12 +1,19 @@
 import TwitterApi from "twitter-api-v2";
 import {Weather} from "../model/types/weather";
 
+// __dirname = ozora-otenki-bot-ts/functions/lib/lib
+const imagesFolderPath = "../functions/src/assets/images";
+
 export const tweet = async (
     twitterClient: TwitterApi,
     weatherInfo: Weather
 ): Promise<void> => {
-  // WARN:同じ内容をツイートするとAPIの仕様によりエラー
-  await twitterClient.v2.tweet(createTweetText(weatherInfo));
+  // WARN: 同じ内容をツイートするとAPIの仕様によりエラー
+  // メディアツイートはv1を推奨とのこと
+  const text = createTweetText(weatherInfo);
+  const mediaId = await generateMediaId(twitterClient);
+
+  await twitterClient.v1.tweet(text, {media_ids: mediaId});
 };
 
 export const createTweetText = (weatherInfo: Weather): string => {
@@ -14,4 +21,21 @@ export const createTweetText = (weatherInfo: Weather): string => {
     `${weatherInfo.todayTelop} ${weatherInfo.todayMaxTemperature}`;
 
   return contentText;
+};
+
+export const generateMediaId = async (
+    twitterClient: TwitterApi
+): Promise<string> => {
+  const randomId =
+    generateRandomNumber(`${process.env.NUMBER_OF_IMAGES}`).toString();
+  const imagesPath = `${imagesFolderPath}/otenki${randomId}.jpg`;
+
+  return twitterClient.v1.uploadMedia(imagesPath);
+};
+
+// 画像id(=0~20)の範囲で整数を生成
+export const generateRandomNumber = (stImages: string) :number => {
+  const numImages = Number(stImages);
+
+  return Math.floor(Math.random() * numImages);
 };
